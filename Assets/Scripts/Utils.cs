3af827f32c5fb8059c6432b8b7008c759aea8ade
Денис Коростелев <denrus99@gmail.com>
2022-11-30 23:57:48 +0500

    Initial commit

---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utils : MonoBehaviour
+{
+    public bool IsWireCube = false;
+    public Color WireColor = Color.green;
+    private void OnDrawGizmos()
+    {
+        // if (IsWireCube)
+        // {
+        //     Gizmos.color = WireColor;
+        //     Gizmos.DrawWireCube(transform.position, transform.localRotation * transform.localScale);
+        // }
+    }
+}
